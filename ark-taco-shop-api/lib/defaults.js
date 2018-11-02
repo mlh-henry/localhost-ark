ee23382a7506963c4540f6cae4042f2fe0886af0
Halisson Bruno <halissonvit@gmail.com>
2018-11-01 21:16:35 -0300

    change default values to make bootstrap easier

--- ark-taco-shop-api/lib/defaults.js
+++ ark-taco-shop-api/lib/defaults.js
@@ -8,7 +8,7 @@ module.exports = {
     port: process.env.ARK_INVENTORY_API_DATABASE_PORT || 5432,
     username: process.env.ARK_INVENTORY_API_DATABASE_USER || 'ark',
     password: process.env.ARK_INVENTORY_API_DATABASE_PASSWORD || 'password',
-    database: process.env.ARK_INVENTORY_API_DATABASE_NAME || 'ark_bridgechain'
+    database: process.env.ARK_INVENTORY_API_DATABASE_NAME || 'ark_testnet'
   },
   server: {
     enabled: process.env.ARK_INVENTORY_API_SERVER_ENABLED || true,
